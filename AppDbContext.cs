74559656d33f2bfa25fe8d0a9dcd0a867f529b43
Albert Hu <oop121.ah@gmail.com>
2026-03-24 23:34:37 +0800

    新增product.cs及AppDbContext.cs

---
+++ AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InventoryAppLocalDb_EF
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasPrecision(10, 2);
+
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Id)
+                .UseIdentityAlwaysColumn();
+        }
+    }
+}
--- Product.cs
+++ Product.cs
@@ -1,14 +1,37 @@
-﻿namespace InventoryAppLocalDb_EF;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace InventoryAppLocalDb_EF;
+
+[Table("products")]
 public class Product
 {
+    [Column("id")]
     public int Id { get; set; }
+
+    [Column("name")]
+    [Required]
+    [MaxLength(100)]
     public string Name { get; set; } = "";
+
+    [Column("price")]
+    [Range(0, 9999999)]
     public double Price { get; set; }
+
+    [Column("stock")]
     public int Stock { get; set; }
+
+    [Column("category")]
+    [MaxLength(50)]
     public string Category { get; set; } = "";
 
+    [Column("created_at")]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
     public bool IsLowStock => Stock < 10;
+
+    // 保留原本的建構子
+    public Product() { }
 
     public Product(int id, string name, double price, int stock, string category)
     {
@@ -18,5 +41,4 @@ public class Product
         Stock = stock;
         Category = category;
     }
-    public Product() { }
 }
